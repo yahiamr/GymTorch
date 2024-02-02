cdef387a6ba5fd71bc2c04e84abefc68092e5ac9
Yahia Abukhalil <yahiamrvc@gmail.com>
2024-02-02 23:41:50 +0200

    refined the code for best practices and modularity and readibility

--- 1stgym.py
+++ 1stgym.py
@@ -1,17 +1,26 @@
 import gymnasium as gym
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
-env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human',desc=generate_random_map(size=8))
+# Generate a random map for the FrozenLake environment
+size = 8  # Define the size of the map
+random_map = generate_random_map(size=size)
 
-for _ in range(5):
+# Create the environment with the generated map
+env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human', desc=random_map)
+
+for episode in range(5):
     state = env.reset()
-    for _ in range(100):
+    done = False
+    step = 0
+
+    while not done and step < 100:
         action = env.action_space.sample()  # Select a random action
-        state, reward, done,_, info = env.step(action)
+        state, reward, done, _, info = env.step(action)  # Adapted for potential extra value
         env.render()
+        step += 1
+
         if done:
+            print(f"Episode ended with reward: {reward}")
             break
 
 env.close()
-
-
