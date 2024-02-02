89e6e5ad30696925eed27da72ddda41362b17589
Yahia Abukhalil <yahiamrvc@gmail.com>
2024-02-02 23:35:50 +0200

    used random generated map for various sizes

--- 1stgym.py
+++ 1stgym.py
@@ -1,14 +1,16 @@
 import gymnasium as gym
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
-env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human')
+env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human',desc=generate_random_map(size=8))
 
-state = env.reset()
-for _ in range(100):
-    action = env.action_space.sample()  # Select a random action
-    state, reward, done,_, info = env.step(action)
-    env.render()
-    if done:
-        break
+for _ in range(5):
+    state = env.reset()
+    for _ in range(100):
+        action = env.action_space.sample()  # Select a random action
+        state, reward, done,_, info = env.step(action)
+        env.render()
+        if done:
+            break
 
 env.close()
 
