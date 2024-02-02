86ddc2f93d0d9e3d5e9daf9f92f049c30d2d23d2
Yahia Abukhalil <yahiamrvc@gmail.com>
2024-02-02 23:28:48 +0200

    used gymnasium instead of gym

--- 1stgym.py
+++ 1stgym.py
@@ -1,29 +1,15 @@
-import gym
+import gymnasium as gym
 
 env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human')
-state_size = env.observation_space.n
-action_size = env.action_space.n
 
-for episode in range(5):  # Run 5 episodes
-    state = env.reset()  # Reset the environment to the starting state
-    done = False
-    print(f"*** Episode {episode+1} ***\n\n\n\n")
-    step = 0
-    total_reward = 0  # Track total reward in the episode
+state = env.reset()
+for _ in range(100):
+    action = env.action_space.sample()  # Select a random action
+    state, reward, done,_, info = env.step(action)
+    env.render()
+    if done:
+        break
 
-    while not done:
-        env.render()  # Display the current environment state
-        action = env.action_space.sample()  # Take a random action
-        state, reward, done,_extra, info = env.step(action)  # Apply the action
-        total_reward += reward
-        print(f"State: {state}, Reward: {reward}, Done: {done}, Info: {info}")
-        
-        step += 1
-        if done:
-            if total_reward > 0:
-                print("\n\nReached the goal!")
-            else:
-                print("\n\nFell into a hole.")
-            break
+env.close()
 
-    print(f"Episode finished after {step} steps with total reward: {total_reward}\n")
+
