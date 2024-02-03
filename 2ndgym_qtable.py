44ea61fd520f0af4e35c751faeb146d7dc3795c8
yahiamr <146677961+yahiamr@users.noreply.github.com>
2024-02-03 23:34:51 +0200

    Update 2ndgym_qtable.py initialised qtable parameters

--- 2ndgym_qtable.py
+++ 2ndgym_qtable.py
@@ -6,3 +6,8 @@ import numpy as np
 size = 8
 random_map = generate_random_map(size=size)
 env = gym.make('FrozenLake-v1', desc=random_map, is_slippery=True, render_mode=None)
+
+# Q-table initialization
+state_size = env.observation_space.n
+action_size = env.action_space.n
+Q_table = np.zeros((state_size, action_size))
