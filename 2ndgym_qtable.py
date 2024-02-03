43acee9cf4b27f7787eeb701d8e673140a373b91
yahiamr <146677961+yahiamr@users.noreply.github.com>
2024-02-03 23:33:11 +0200

    Update 2ndgym_qtable.py added gym and env setup

--- 2ndgym_qtable.py
+++ 2ndgym_qtable.py
@@ -1,1 +1,8 @@
-#to solve gym with qtable
+import gymnasium as gym
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+import numpy as np
+
+# Environment setup
+size = 8
+random_map = generate_random_map(size=size)
+env = gym.make('FrozenLake-v1', desc=random_map, is_slippery=True, render_mode=None)
