881ace29d30aaefe3336221665371496716f5930
yahiamr <146677961+yahiamr@users.noreply.github.com>
2024-02-15 23:42:15 +0200

    Update dqn_frozenlake.py

--- dqn_frozenlake.py
+++ dqn_frozenlake.py
@@ -1,1 +1,15 @@
-#for solving frozen lake using dqn
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class DQN(nn.Module):
+    def __init__(self, state_size, action_size, hidden_size=50):
+        super(DQN, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(state_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, action_size)
+        )
+
+    def forward(self, x):
+        return self.network(x)
